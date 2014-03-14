73e8899e1360ec8c9aff44df87b6697cd8b315d8
Tanut Viteevanich <aon@addresstalk.com>
2014-03-15 00:44:26 +0700

    Ignore file.

--- inc/config.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-@mysql_connect("localhost","admin_football","football!@#$%") or die(mysql_error());
-@mysql_select_db("admin_football") or die(mysql_error());
-@mysql_query("SET NAMES utf8");
-?>
